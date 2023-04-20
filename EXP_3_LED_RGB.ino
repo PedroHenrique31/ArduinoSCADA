59113f517968bdc4edae1a2a40eb0eb02bdd6512
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2023-04-20 12:14:50 -0300

    Descobri o problema do led ficar piscando, eram 3 linhas de codigo que estavam soltas, agora é ajustar o sensor DHT11 que esta estranho.

--- EXP_3_LED_RGB.ino
+++ EXP_3_LED_RGB.ino
@@ -2,14 +2,19 @@
   Autor: Pedro Henrique Cerneiro de Araújo        RA:22108287
   Autor: Bruna Stephanie Soares Gonçalves         RA:21707921
 */
+
+#include <DHT.h> // Biblioteca do sensor DHT 
+#include <DHT_U.h>
 #include "SimpleModbusSlave.h"
-/*#define saida1
-#define saida2
-#define saida3*/
+#define DHT_PIN A3
 #define LED_AZ 9
 #define LED_VD 10
 #define LED_VM 11
 #define CHAVE  12
+//DHT DHT11; // declara um objeto do tipo DHT chamado DHT11
+
+
+
 enum
 {
   VALOR_POT1,
@@ -22,6 +27,8 @@ enum
   VALOR_PWMG,
   VALOR_PWMB,
   MAN_AUTO,
+  VALORT_DHT11, //Valor temperatura no DHT11
+  VALORH_DHT11, //Valor umidade no DHT11
   HOLDING_REGS_SIZE
 };
 unsigned int holdingRegs[HOLDING_REGS_SIZE];
@@ -40,19 +47,20 @@ void loop() {
   modbus_update();
   int valor1,valor2,valor3;
 
+  /* Lê dados das entradas anal[ogicas e digitais e atribui cada uma a um registrador */
   holdingRegs[VALOR_POT1] = analogRead(A0);
   holdingRegs[VALOR_POT2] = analogRead(A1);
   holdingRegs[VALOR_POT3] = analogRead(A2);
   holdingRegs[MAN_AUTO] = digitalRead(CHAVE);
- 
+  //DHT11.read11(DHT_PIN); 
+  //holdingRegs[VALORT_DHT11]=DHT11.temperature;
+  //holdingRegs[VALORH_DHT11]=DHT11.humidity;
+
+  /* Mapeia os valores analógicos (de 0 a 1023) para valores digitais (De 0 a 255) para escrever nas saídas depois */
   valor1=map(holdingRegs[VALOR_POT1],0,1023,0,255);
   valor2=map(holdingRegs[VALOR_POT2],0,1023,0,255);
   valor3=map(holdingRegs[VALOR_POT3],0,1023,0,255);
-
-
-  /* int estado0 = holdingRegs[VALOR_POT1];
-   int estado1 = holdingRegs[VALOR_POT2];
-   int estado2 = holdingRegs[VALOR_POT3];   */  
+ 
 
 if (holdingRegs[MAN_AUTO] == 0) {
    
@@ -62,7 +70,7 @@ if (holdingRegs[MAN_AUTO] == 0) {
     holdingRegs[VALOR_PWMG] = map (holdingRegs[VALOR_POT2], 0, 1023, 0, 255);
     analogWrite (LED_VD, holdingRegs [VALOR_PWMG]);  
 
-    holdingRegs[VALOR_PWMR] = map (holdingRegs[VALOR_POT3], 0, 1023, 0, 255);
+    holdingRegs[VALOR_PWMB] = map (holdingRegs[VALOR_POT3], 0, 1023, 0, 255);
     analogWrite (LED_AZ, holdingRegs [VALOR_PWMB]);
    
   }
@@ -78,21 +86,11 @@ if (holdingRegs[MAN_AUTO] == 0) {
     analogWrite (LED_AZ, holdingRegs [VALOR_PWMB]);    
  
   }
-// ==============================================================
-// retirar isso daqui depois, mas pra enquanto tá top  
-/*    Serial.print("Estado do botão: ");
-    Serial.print(estado0);
-    Serial.print(" ");    
-    Serial.print(estado1);
-    Serial.print(" ");
-    Serial.print(estado2);
-    Serial.println(" ");
-
-//==============================================================
- */  
+  /*
+  // é por causa disso aqui que o codigo ta sempre com algum valor aceso
   analogWrite(LED_AZ,valor1);
   analogWrite(LED_VD,valor2);
   analogWrite(LED_VM,valor3);
-
+*/
 
 }
